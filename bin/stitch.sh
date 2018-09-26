aa9446927cdf402529490a1310bb455df32d0549
Stefan Knorr <sknorr@suse.de>
2018-09-26 14:46:00 +0200

    Add stitch.sh to stitch together config files

---
+++ bin/stitch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# $1 - Directory with config files
+# $2 - Output file (optional, if not set, outputs to stdout)
+
+out() {
+  >&2 echo "$1"
+  exit 1
+}
+
+starlet='xmlstarlet'
+
+[[ ! -d $1 ]] && out "Directory $1 does not exist"
+
+indir=$1
+
+cd $indir
+
+for file in *.xml; do
+  [[ $(xmllint --noout --noent $file) ]] && out "$outdir/$file is not well-formed."
+done
+
+outfile='<?xml version="1.0" encoding="UTF-8"?>\n<docservconfig>\n\n'
+
+for file in *.xml; do
+  outfile+=$($starlet sel -t -c "/*" $file)
+  outfile+='\n'
+done
+
+outfile+='\n</docservconfig>\n'
+
+if [[ $2 ]]; then
+  echo -e "$outfile" > $2
+else
+  echo -e "$outfile"
+fi
+
+# FIXME: Now validate according to DTD/RNG (which we currently don't have)

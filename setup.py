094086669296be8e86c1b4f1c9a3d58499667fd4
Sven Seeberg <sven@geeq.de>
2018-10-05 19:11:34 +0200

    Fix syntax typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="docserv",
-    version="0.1.4",
+    version="0.9.4",
     packages=find_packages(),
     scripts=['bin/docserv', 'bin/docserv-stitch'],
     install_requires=[],

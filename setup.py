b949deb326180840db41ee4e4a520f2d477b0a2b
Sven Seeberg <sven@geeq.de>
2018-10-10 15:46:07 +0200

    Bump version to 0.9.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="docserv",
-    version="0.9.6",
+    version="0.9.7",
     packages=find_packages(),
     scripts=['bin/docserv', 'bin/docserv-stitch'],
     install_requires=[],

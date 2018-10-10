d24c2d1627e33f1206f42843c33e52ed82aad557
Sven Seeberg <sven@geeq.de>
2018-10-10 16:33:36 +0200

    Remove Python v3.3 support

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

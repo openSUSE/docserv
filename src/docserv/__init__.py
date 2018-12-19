72339e5a4202bbbad53a9afc9b2ad5cad4496026
Sven Seeberg <mail@sven-seeberg.de>
2018-12-19 16:45:22 +0100

    Add python headers

---
+++ src/docserv/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Sven Seeberg, Thomas Schraitle, Stefan Knorr"
+__copyright__ = "Copyright 2018, SUSE Linux LLC"
+
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Thomas Schraitle"
+__email__ = "doc-team@suse.de"
+__status__ = "Production"

66ebb6a92bb1b7c57c7430a1c537974be96280c9
Stefan Knorr <sknorr@suse.de>
2019-05-24 18:14:15 +0200

    schema: Allow adding documents to categories (#38)
    also validates the following properties of categories:
    * language codes within category are not duplicated
    * category ID uniqueness
    * category reference validity

---
+++ share/validate-product-config/checks/check-lang-code-in-category.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# make sure each language code appears only once within a given product's
+# category names
+
+file=$1
+
+categories=$($starlet sel -t -v "count(//category)" $file)
+for category in $(seq 1 "$categories"); do
+  current_id=$($starlet sel -t -v "//category["$category"]/@categoryid" $file | sort)
+  langcodes=$($starlet sel -t -v "//category["$category"]/name/@lang" $file | sort)
+  uniquelangcodes=$(echo -e "$langcodes" | sort -u)
+  [[ ! "$langcodes" == "$uniquelangcodes" ]] && \
+  echo -e \
+    "Some of the name translations of category \"$current_id\" have non-unique lang attributes. Check for occurrences of the following duplicated lang attribute(s) in the name elements of category \"$current_id\": "$(comm -2 -3 <(echo -e "$langcodes") <(echo -e "$uniquelangcodes") | tr '\n' ' ')".\n---"
+done
+
+exit 0

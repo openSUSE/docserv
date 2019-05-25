24c48844dac26b644ac9dd2b8850e33320f209a7
Stefan Knorr <sknorr@suse.de>
2019-05-25 21:51:03 +0200

    stitch: subdeliverables must not be combined with pdf/epub (#63)

---
+++ share/validate-product-config/checks/check-format-subdeliverable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# make sure that deliverables that contain subdeliverables only enable html
+# or single-html as format
+
+file=$1
+
+deliverables=$($starlet sel -t -v "count(//deliverable[subdeliverable])" $file | sort)
+for deliverable in $(seq 1 "$deliverables"); do
+  hasbadformat=$($starlet sel -t -v "(//deliverable[subdeliverable])[$deliverable]/format/@*[local-name(.) = 'epub' or local-name(.) = 'pdf'][. = 'true' or . = '1']" $file)
+  [[ "$hasbadformat" ]] && {
+    setid=$($starlet sel -t -v "(//deliverable[subdeliverable])[$deliverable]/ancestor::docset/@setid" $file)
+    language=$($starlet sel -t -v "(//deliverable[subdeliverable])[$deliverable]/ancestor::language/@lang" $file)
+    dc=$($starlet sel -t -v "(//deliverable[subdeliverable])[$deliverable]/dc" $file)
+    echo -e \
+      "A deliverable that has subdeliverables has PDF or EPUB enabled as a format: docset=$setid/language=$language/deliverable=$dc. subdeliverables are only supported for the formats HTML and Single-HTML.\n---"
+ }
+done
+
+exit 0

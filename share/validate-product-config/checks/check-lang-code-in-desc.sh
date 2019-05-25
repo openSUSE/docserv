db308b05512a9ad1a82dbc37b8cfe692c00c4d36
Stefan Knorr <sknorr@suse.de>
2019-05-25 21:14:20 +0200

    stitch: langcodes in a product's descs must appear only once (#63)

---
+++ share/validate-product-config/checks/check-lang-code-in-desc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# make sure each language code appears only once within a given product's
+# description texts (desc)
+
+file=$1
+
+langcodes=$($starlet sel -t -v "//desc/@lang" $file | sort)
+uniquelangcodes=$(echo -e "$langcodes" | sort -u)
+[[ ! "$langcodes" == "$uniquelangcodes" ]] && \
+echo -e \
+  "Some desc elements have non-unique lang attributes. Check for occurrences of the following duplicated lang attribute(s) in desc elements: "$(comm -2 -3 <(echo -e "$langcodes") <(echo -e "$uniquelangcodes") | tr '\n' ' ')"\n---"
+
+exit 0

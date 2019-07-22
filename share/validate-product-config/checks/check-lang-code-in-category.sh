9e8f1bb02400118b6525e3a14cb62591edb5f90c
Stefan Knorr <sknorr@suse.de>
2019-07-22 10:08:19 +0200

    stitch-schema: Allow category descriptions
    Changes:
    * category/name -> category/language
    * content of category/language is an (optional) description
    * name is now known as "title"

--- share/validate-product-config/checks/check-lang-code-in-category.sh
+++ share/validate-product-config/checks/check-lang-code-in-category.sh
@@ -8,11 +8,11 @@ file=$1
 categories=$($starlet sel -t -v "count(//category)" $file)
 for category in $(seq 1 "$categories"); do
   current_id=$($starlet sel -t -v '//category['"$category"']/@categoryid' $file | sort)
-  langcodes=$($starlet sel -t -v '//category['"$category"']/name/@lang' $file | sort)
+  langcodes=$($starlet sel -t -v '//category['"$category"']/language/@lang' $file | sort)
   uniquelangcodes=$(echo -e "$langcodes" | sort -u)
   [[ ! "$langcodes" == "$uniquelangcodes" ]] && \
   echo -e \
-    "Some of the name translations of category \"$current_id\" have non-unique lang attributes. Check for occurrences of the following duplicated lang attribute(s) in the name elements of category \"$current_id\": "$(comm -2 -3 <(echo -e "$langcodes") <(echo -e "$uniquelangcodes") | tr '\n' ' ')".\n---"
+    "Some of the name translations of category \"$current_id\" have non-unique lang attributes. Check for occurrences of the following duplicated lang attribute(s) in the language elements of category \"$current_id\": "$(comm -2 -3 <(echo -e "$langcodes") <(echo -e "$uniquelangcodes") | tr '\n' ' ')".\n---"
 done
 
 exit 0

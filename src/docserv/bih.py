3ab3c2b8465a1a824bcc4d7388dc1729e52c3df4
Stefan Knorr <sknorr@suse.de>
2019-07-31 03:38:40 +0200

    ds2: internal mode should not be determined by language = yes
    (fix for a copypasta mistake)

--- src/docserv/bih.py
+++ src/docserv/bih.py
@@ -106,7 +106,7 @@ class BuildInstructionHandler:
             commands[n] = {}
             commands[n]['cmd'] = "docserv-build-navigation %s --product=\"%s\" --docset=\"%s\" --stitched-config=\"%s\" --ui-languages=\"%s\" --cache-dir=\"%s\" --template-dir=\"%s\" --output-dir=\"%s\"" % (
                 "--internal-mode" if self.config['targets'][self.build_instruction['target']
-                                                             ]['languages'] == "yes" else "",
+                                                             ]['internal'] == "yes" else "",
                 self.build_instruction['product'],
                 self.build_instruction['docset'],
                 self.stitch_tmp_file,

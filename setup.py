95b49fed2ff70fe2cf804e51c4f283f5fb645525
Stefan Knorr <sknorr@suse.de>
2019-08-02 15:48:00 +0200

    Update setuppy

--- setup.py
+++ setup.py
@@ -33,11 +33,18 @@ setup(
             'docserv = docserv.docserv:main',
         ]
     },
-    scripts=['bin/docserv-stitch', 'bin/docserv-dirs', 'bin/docserv-createconfig', 'bin/docserv-build-navigation', 'bin/docserv-dchash'],
+    scripts=[
+        'bin/docserv-stitch',
+        'bin/docserv-dirs',
+        'bin/docserv-create-archive',
+        'bin/docserv-createconfig',
+        'bin/docserv-build-navigation',
+        'bin/docserv-dchash',
+    ],
     install_requires=[],
     data_files=[
         ('/usr/share/docserv/example/', ['config/docserv.ini']),
-        ('/usr/share/docserv/example/config/config.d/', glob('config/config.d/sample_product.xml')),
+        ('/usr/share/docserv/example/config/config.d/', glob('config/config.d/*.xml')),
         # template can be .html or .php (and potentially more in the future)
         ('/usr/share/docserv/example/template/', glob('config/templates/template.*')),
         ('/usr/share/docserv/example/template/', ['config/templates/translation.xml']),
@@ -45,11 +52,13 @@ setup(
         ('/usr/share/docserv/example/template/res/', glob('config/templates/res/*')),
         ('/usr/share/docserv/validate-product-config/', ['share/validate-product-config/product-config-schema.rnc']),
         # there are bound to be more checks over time in here and they may include XSLT-based ones
+        ('/usr/share/docserv/validate-product-config/', glob('share/validate-product-config/global-check-*')),
         ('/usr/share/docserv/validate-product-config/checks/', glob('share/validate-product-config/checks/check-*.sh')),
         ('/usr/share/docserv/validate-product-config/checks/', glob('share/validate-product-config/checks/check-*.xsl')),
         # both of these currently contain a single .xsl file but there may be more in the future
         ('/usr/share/docserv/simplify-product-config/', glob('share/simplify-product-config/*.xsl')),
-        ('/usr/share/docserv/build-navigation/',  glob('share/build-navigation/*.xsl')),
+        ('/usr/share/docserv/build-navigation/', glob('share/build-navigation/*.xsl')),
+        ('/usr/share/docserv/build-navigation/web-resources/', glob('share/build-navigation/web-resources/*')),
         ('/usr/lib/systemd/system/', ['systemd/docserv@.service']),
     ],
     author="SUSE Documentation Team",
@@ -57,7 +66,7 @@ setup(
     description="Build server for DAPS documentation",
     license="GPL-3.0-or-later",
     keywords="DAPS build documentation",
-    url="http://github.com/SUSEdoc/",
+    url="http://github.com/openSUSE/docserv",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Documentation',

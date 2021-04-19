728397514552cdd9a7fc045b828d1a997533e1fa
Stefan Knorr <sknorr@suse.de>
2021-04-19 16:38:03 +0200

    docserv/rest: Properly catch error when users submit invalid JSON to API

--- src/docserv/rest.py
+++ src/docserv/rest.py
@@ -7,8 +7,8 @@ logger = logging.getLogger('docserv')
 
 
 class RESTServer(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
+    def _set_headers(self, code=200):
+        self.send_response(code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
@@ -26,13 +26,20 @@ class RESTServer(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         # [{"docset": "15ga", "lang": "en-us", "product": "sles", "target": "external"}. ]
         post_data = self.rfile.read(content_length)
-        build_jobs = json.loads(post_data)
-        for job in build_jobs:
-            if self.server.docserv.queue_build_instruction(job):
-                logger.info("Queueing %s", json.dumps(job))
-            else:
-                logger.info("Not queueing %s", json.dumps(job))
-        self._set_headers()
+        try:
+            build_jobs = json.loads(post_data)
+            for job in build_jobs:
+                if self.server.docserv.queue_build_instruction(job):
+                    logger.info("Queueing %s", json.dumps(job))
+                else:
+                    logger.info("Not queueing %s", json.dumps(job))
+            self._set_headers()
+        except json.decoder.JSONDecodeError:
+            # do not print the details of the request, when we add an
+            # authentication thing here, we may not be able to scrub the
+            # password from a malformed request easily.
+            logger.warning("Invalid JSON data submitted to REST API as build instruction. Ignoring.")
+            self._set_headers(400)
 
 
 class ThreadedRESTServer(ThreadingMixIn, HTTPServer):

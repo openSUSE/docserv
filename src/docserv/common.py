c84510f0e2c9773e3120288309172d306503631e
Tom Schraitle <tom_schr@web.de>
2024-04-26 15:08:27 +0200

    Introduce PROJECT_DIR/DOCSERV_PROJECT_DIR,

--- src/docserv/common.py
+++ src/docserv/common.py
@@ -6,4 +6,8 @@ CONF_DIR = os.getenv('DOCSERV_CONFIG_DIR', "/etc/docserv/")
 SHARE_DIR = os.getenv('DOCSERV_SHARE_DIR', "/usr/share/docserv/")
 CACHE_DIR = os.getenv('DOCSERV_CACHE_DIR', "/var/cache/docserv/")
 
+#: Contains the root directory of a project dir, usually docserv-devel,
+#: when used from Git
+PROJECT_DIR = os.getenv('DOCSERV_PROJECT_DIR', "/tmp/docserv-devel")
+
 DOCSERV_CODE_DIR = os.path.join(os.path.dirname(__file__))

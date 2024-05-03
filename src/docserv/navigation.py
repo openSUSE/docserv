e9c965d0de0860561a0f8519c375bc635ccf7368
Tom Schraitle <tom_schr@web.de>
2024-05-03 08:36:01 +0200

    jupiterlab -> juypter

--- src/docserv/navigation.py
+++ src/docserv/navigation.py
@@ -233,7 +233,7 @@ def render_and_save(env, outputdir: str, bih) -> None:
                 ("hewlett-packard-enterprise", dict(isTRD=True, partner="Hewlett Packard Enterprise")), #
                 ("hp", dict(isTRD=True, partner="HP")), # l
                 ("ibm", dict(isTRD=True, partner="IBM")), # l
-                ("jupiterlab", dict(isTRD=True, partner="JupiterLab")), # l
+                ("jupyter", dict(isTRD=True, partner="JupiterLab")), # l
                 # ("kubecost", dict(isTRD=True, partner="Kubecost")), # l
                 ("kubeflow", dict(isTRD=True, partner="Kubeflow")), #
                 ("lenovo", dict(isTRD=True, partner="Lenovo")),

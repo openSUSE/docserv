30feed1cced69d75d67dfbab729f17a8fab67cb2
Tom Schraitle <toms@suse.de>
2024-09-09 10:21:20 +0200

    Prepare for /metadata route to be handled

--- src/docserv/rest.py
+++ src/docserv/rest.py
@@ -23,9 +23,20 @@ class RESTServer(BaseHTTPRequestHandler):
                 bytes(json.dumps(self.server.docserv.deliverables), "utf-8"))
 
     def do_POST(self):
+        logger.debug("Received POST request")
+
+        if self.path == "/":
+            self.handle_build()
+        elif self.path == "/metadata":
+            self.handle_metadata()
+        else:
+            self.send_error(404, "Not Found")
+
+    def handle_build(self):
         content_length = int(self.headers['Content-Length'])
         # [{"docset": "15ga", "lang": "en-us", "product": "sles", "target": "external"}. ]
         post_data = self.rfile.read(content_length)
+        logger.debug("Received POST data: %s", post_data)
         try:
             build_jobs = json.loads(post_data)
             for job in build_jobs:
@@ -33,13 +44,30 @@ class RESTServer(BaseHTTPRequestHandler):
                     logger.info("Queueing %s", json.dumps(job))
                 else:
                     logger.info("Not queueing %s", json.dumps(job))
-            self._set_headers()
+            # self._set_headers()
+            return self.send_response(200)
         except json.decoder.JSONDecodeError:
             # do not print the details of the request, when we add an
             # authentication thing here, we may not be able to scrub the
             # password from a malformed request easily.
             logger.warning("Invalid JSON data submitted to REST API as build instruction. Ignoring.")
-            self._set_headers(400)
+            self.send_error(400, "Invalid JSON data")
+
+    def handle_metadata(self):
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length).decode('utf-8')
+        data = json.loads(data)
+        logger.debug("Received POST data: %s", data)
+        # Process the data and respond
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        response = ["Building metadata for:"]
+        for item in data:
+            response.append(
+                f" * {item['target']}:{item['product']}/{item['docset']}/{item['lang']}\n"
+                )
+        self.wfile.write("\n".join(response).encode('utf-8'))
 
 
 class ThreadedRESTServer(ThreadingMixIn, HTTPServer):

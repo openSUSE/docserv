3771284aa193dc25efde58263dcd05b3435bfbcc
Tom Schraitle <toms@suse.de>
2024-11-06 16:40:00 +0100

    Raise version to 6.10.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="docserv",
-    version="6.10",
+    version="6.10.1",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points={

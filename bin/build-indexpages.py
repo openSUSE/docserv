bc08e1371830d50c417b0e0a885c4d5bd199f330
Tom Schraitle <tom_schr@web.de>
2024-12-04 16:34:32 +0100

    Remove shlex.quote

--- bin/build-indexpages.py
+++ bin/build-indexpages.py
@@ -342,7 +342,7 @@ async def run_command(command: str) -> int:
         The return code of the command.
     """
     process = await asyncio.create_subprocess_shell(
-        shlex.quote(command),
+        command,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )

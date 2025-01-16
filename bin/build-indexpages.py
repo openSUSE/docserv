dd0a68c14c6d8737666c2a1c19040b03dc508fd2
Tom Schraitle <tom_schr@web.de>
2025-01-16 17:40:47 +0100

    Add convert2bool() function
    SIgnature: convert2bool(value: str | bool) -> bool:

--- bin/build-indexpages.py
+++ bin/build-indexpages.py
@@ -621,6 +621,32 @@ class DapsError(ScriptBaseError):
 
 
 # --- Functions
+def convert2bool(value: str | bool) -> bool:
+    """Convert a string or bool into a boolean
+
+    :param value: The value to convert to a boolean
+        Valid values are: True, "yes", "true", "1", "on" for True and
+        False, "no", "false", "0", "off" for False.
+    :return: The boolean value
+    :raises ValueError: If the value is not a valid boolean
+    """
+    map = {
+        "yes": True,
+        "true": True,
+        "1": True,
+        "on": True,
+        "no": False,
+        "false": False,
+        "0": False,
+        "off": False,
+    }
+    value = str(value).lower()
+    if value in map:
+        return map[value]
+
+    raise ValueError(f"Invalid boolean value: {value}")
+
+
 def read_ini_file(inifile: Path, target="doc-suse-com") -> dict[str, Optional[str]]:
     """
     Read an INI file and return its content as a dictionary
